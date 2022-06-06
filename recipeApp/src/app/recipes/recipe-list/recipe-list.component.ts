a98911a4d33e1d4a74a537d231c09c843f6b9a54
ridvanms <ridvans@abv.bg>
2022-06-06 15:45:54 +0300

    uploading

--- recipeApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipeApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recope.mode';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test Recipe',
+      'This is a simple test',
+      'https://th.bing.com/th/id/R.89b0b4650f29a17030bad22c9e883f61?rik=293O790OghSJ5Q&pid=ImgRaw&r=0'
+    ),
+    new Recipe(
+      'A test Recipe',
+      'This is a simple test',
+      'https://th.bing.com/th/id/R.89b0b4650f29a17030bad22c9e883f61?rik=293O790OghSJ5Q&pid=ImgRaw&r=0'
+    ),
+  ];
   constructor() {}
 
   ngOnInit(): void {}
