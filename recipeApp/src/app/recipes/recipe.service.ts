23a86a2fb2e0ec4335af31996c8da9d77c4f2e13
ridvanms <ridvans@abv.bg>
2022-06-16 23:43:16 +0300

    adding ingredients to shopping list

--- recipeApp/src/app/recipes/recipe.service.ts
+++ recipeApp/src/app/recipes/recipe.service.ts
@@ -1,32 +1,40 @@
 import { EventEmitter, Injectable, Output } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 import { Recipe } from './recipe.mode';
 @Injectable({
   providedIn: 'root',
 })
 export class RecipeService {
-  constructor() {}
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
       'Recipe',
       'First Recipe',
-      'https://th.bing.com/th/id/R.89b0b4650f29a17030bad22c9e883f61?rik=293O790OghSJ5Q&pid=ImgRaw&r=0'
+      'https://th.bing.com/th/id/R.89b0b4650f29a17030bad22c9e883f61?rik=293O790OghSJ5Q&pid=ImgRaw&r=0',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
       'Another Recipe',
       'Testing the second one',
-      'https://th.bing.com/th/id/R.89b0b4650f29a17030bad22c9e883f61?rik=293O790OghSJ5Q&pid=ImgRaw&r=0'
+      'https://th.bing.com/th/id/R.89b0b4650f29a17030bad22c9e883f61?rik=293O790OghSJ5Q&pid=ImgRaw&r=0',
+      [new Ingredient('Bean', 2), new Ingredient('Buns', 23)]
     ),
     new Recipe(
       'One More Recipe',
       'Testing the last',
-      'https://th.bing.com/th/id/R.89b0b4650f29a17030bad22c9e883f61?rik=293O790OghSJ5Q&pid=ImgRaw&r=0'
+      'https://th.bing.com/th/id/R.89b0b4650f29a17030bad22c9e883f61?rik=293O790OghSJ5Q&pid=ImgRaw&r=0',
+      [new Ingredient('Meat', 2), new Ingredient('French Fries', 12)]
     ),
   ];
+  constructor(private slService: ShoppingListService) {}
 
   getRecipe() {
     return this.recipes.slice();
+  }
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngridients(ingredients);
   }
 }
--- recipeApp/src/app/shopping-list/shopping-list.service.ts
+++ recipeApp/src/app/shopping-list/shopping-list.service.ts
@@ -18,4 +18,11 @@ export class ShoppingListService {
     this.ingredients.push(ingredient);
     this.ingredientsChanged.emit(this.ingredients.slice());
   }
+  addIngridients(ingredients: Ingredient[]) {
+    // for(let ingredient of ingredients){
+    //   this.addingIngredient(ingredient);
+    // }
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
