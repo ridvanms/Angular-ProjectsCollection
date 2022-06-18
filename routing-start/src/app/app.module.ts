813088807a4ac4c950d351ce48ff7cf673e015c0
ridvanms <ridvans@abv.bg>
2022-06-18 17:43:36 +0300

    changes

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -16,7 +16,8 @@ const appRoutes: Routes = [
   { path: "", component: HomeComponent },
   { path: "users", component: UsersComponent },
   { path: "users/:id/:name", component: UsersComponent },
-  { path: "servers", component: ServerComponent },
+  { path: "servers", component: ServersComponent },
+  { path: "servers/:id/edit", component: EditServerComponent },
 ];
 
 @NgModule({
