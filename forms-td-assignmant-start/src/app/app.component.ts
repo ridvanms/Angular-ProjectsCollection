5adb96a56acd14a4b00d6d87069e7886e92d5087
ridvanms <ridvans@abv.bg>
2022-07-09 23:44:29 +0300

    uploading from-assagnment

--- forms-td-assignmant-start/src/app/app.component.ts
+++ forms-td-assignmant-start/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+// import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  defaultValue = 'Advanced';
 
+  generateEmail(form: NgForm) {
+    const suggestedName = 'Koko@gmail.com';
+    form.form.patchValue({ userData: { email: suggestedName } });
+  }
 }
