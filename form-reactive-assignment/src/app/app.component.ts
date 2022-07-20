ab6f093ee306065f28be89c9a03f6013e25b7f83
ridvanms <ridvans@abv.bg>
2022-07-21 00:27:18 +0300

    updating css  component and html component  ...

--- form-reactive-assignment/src/app/app.component.ts
+++ form-reactive-assignment/src/app/app.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  signupForm: FormGroup = new FormGroup({});
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+    });
+    this.signupForm.setValue({
+      email: 'max@abv.bg',
+    });
+  }
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+}
