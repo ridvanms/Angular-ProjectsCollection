8adc63d1f089b1db1768ecb1a1eb54020e8d6554
ridvanms <ridvans@abv.bg>
2022-07-23 09:23:00 +0300

    async validators

--- form-reactive-assignment/src/app/app.module.ts
+++ form-reactive-assignment/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CustomValidators } from './custom-validators';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, ReactiveFormsModule],
-  providers: [],
+  providers: [CustomValidators],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- form-reactive-assignment/src/app/custom-validators.ts
+++ form-reactive-assignment/src/app/custom-validators.ts
@@ -1,10 +1,25 @@
 // import {  FormControl } from "@angular/forms"
 
-// export class CustomValidators {
-//   static: invalidProjectName(control: FormControl): {[s: string]:boolean}{
-//     if(control.value === "Test") {
-//       return {"invalidProjectName": true};
-//     }
-//     return null
-//   }
-// }
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+export class CustomValidators {
+  //   static: invalidProjectName(control: FormControl): {[s: string]:boolean}{
+  //     if(control.value === "Test") {
+  //       return {"invalidProjectName": true};
+  //     }
+  //     return null
+  //   }
+  // adding async
+  static asyncInvalidProjectEmail(control: FormControl) {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'Test@abv.bg') {
+          resolve({ invalidProjectEmail: true });
+        } else {
+          resolve(null);
+        }
+      }, 2000);
+    });
+  }
+}
