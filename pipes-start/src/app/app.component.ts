7c51d4ca3cd718df192e1fc259895c6a8992caf3
ridvanms <ridvans@abv.bg>
2022-07-27 11:14:08 +0300

    udnerstanding the 'async' Pipe

--- pipes-start/src/app/app.component.ts
+++ pipes-start/src/app/app.component.ts
@@ -6,6 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  appStatus = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('stable');
+    }, 2000);
+  });
   servers = [
     {
       instanceType: 'medium',
@@ -44,5 +49,13 @@ export class AppComponent {
       'list-group-item-warning': server.status === 'offline',
       'list-group-item-danger': server.status === 'critical',
     };
+  }
+  onAddData() {
+    this.servers.push({
+      instanceType: 'small',
+      name: 'Developing new Data',
+      status: 'stable',
+      started: new Date(15, 1, 2017),
+    });
   }
 }
