c63dbb11d8ccd902f4e02c8052efdd17ee555ed8
ridvanms <ridvans@abv.bg>
2022-08-01 17:31:19 +0300

    Sending Login Request

--- recipeApp/src/app/auth/aut.component.ts
+++ recipeApp/src/app/auth/aut.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+
+import { AuthResponseData, AuthService } from './auth.service';
 
 @Component({
   selector: 'auth',
@@ -24,22 +26,26 @@ export class AuthComponent {
     const email = form.value.email;
     const password = form.value.password;
 
+    let authObs: Observable<AuthResponseData>;
+
     this.isLoading = true;
 
     if (this.isLoggedIn) {
-      //..
+      authObs = this.authService.login(email, password);
     } else {
-      this.authService.signup(email, password).subscribe(
-        (resData) => {
-          console.log(resData);
-          this.isLoading = false;
-        },
-        (error) => {
-          this.error = 'An error occured!';
-          this.isLoading = false;
-        }
-      );
+      authObs = this.authService.signup(email, password);
     }
+    authObs.subscribe(
+      (resData) => {
+        console.log(resData);
+        this.isLoading = false;
+      },
+      (errorMessage) => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
     form.reset();
   }
 }
--- recipeApp/src/app/auth/auth.service.ts
+++ recipeApp/src/app/auth/auth.service.ts
@@ -1,13 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   idToken: string;
   email: string;
   refreshToken: string;
   expiresToken: string;
   localId: string;
-  registered: boolean;
+  registered?: boolean;
 }
 
 @Injectable({
@@ -17,8 +19,32 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAwyCIQQl046CJeT9pdR3WsxNxjl7swuoQ',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError((errorRes) => {
+          let errorMessage = 'An unknown error occurred!';
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+          }
+          switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage = 'This email exists already';
+          }
+          return throwError(errorMessage);
+        })
+      );
+  }
+  login(email: string, password: string) {
     return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAwyCIQQl046CJeT9pdR3WsxNxjl7swuoQ',
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDETaNGIGVg5VVl6khP6oNeL9up6p2j6V0',
       {
         email: email,
         password: password,
