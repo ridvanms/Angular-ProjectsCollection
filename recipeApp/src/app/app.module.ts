b10c00e9003869f152a7e3ae562166c87ba8e1f2
ridvanms <ridvans@abv.bg>
2022-08-07 13:59:56 +0300

    Splitting Modules Correctly

--- recipeApp/src/app/app.module.ts
+++ recipeApp/src/app/app.module.ts
@@ -2,41 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { AppRountingModule } from './app-rounting.module';
-import { RecipeStatrtComponent } from './recipes/recipe-statrt/recipe-statrt.component';
-import { RecipesEditComponent } from './recipes/recipes-edit/recipes-edit.component';
 import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/aut.component';
 import { AuthService } from './auth/auth.service';
 import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner';
-import { User } from './auth/user.model';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { AlertComponent } from './shared/alert/alet.component';
 import { PlaceHolderDirective } from './shared/placeHolder/placeholder.directive';
+import { RecipesModule } from './recipes/recepies.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
+
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    RecipeStatrtComponent,
-    RecipesEditComponent,
+
     AuthComponent,
     LoadingSpinnerComponent,
     AlertComponent,
@@ -48,6 +37,7 @@ import { PlaceHolderDirective } from './shared/placeHolder/placeholder.directive
     ReactiveFormsModule,
     HttpClientModule,
     FormsModule,
+    RecipesModule,
   ],
   providers: [
     RecipeService,
---
+++ recipeApp/src/app/recipes/recepies.module.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeStatrtComponent } from './recipe-statrt/recipe-statrt.component';
+import { RecipesEditComponent } from './recipes-edit/recipes-edit.component';
+import { RecipesComponent } from './recipes.component';
+
+@NgModule({
+  declarations: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeStatrtComponent,
+    RecipesEditComponent,
+  ],
+  imports: [RouterModule, CommonModule, ReactiveFormsModule],
+  exports: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeStatrtComponent,
+    RecipesEditComponent,
+  ],
+})
+export class RecipesModule {}
