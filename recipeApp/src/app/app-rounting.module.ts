5c505ad2485b47e8a6b4b17ca4866ca57ce18440
ridvanms <ridvans@abv.bg>
2022-08-09 14:15:48 +0300

    Preloading Lazy-Lodaed Code

--- recipeApp/src/app/app-rounting.module.ts
+++ recipeApp/src/app/app-rounting.module.ts
@@ -1,18 +1,7 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
-import { RecipesComponent } from './recipes/recipes.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeStatrtComponent } from './recipes/recipe-statrt/recipe-statrt.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipesEditComponent } from './recipes/recipes-edit/recipes-edit.component';
-import { RecipeResolverService } from './recipes/recipe-resolver.service';
-import { Recipe } from './recipes/recipe.mode';
-import { AuthComponent } from './auth/aut.component';
-import { AuthGuard } from './auth/auth.guard';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { SharedModule } from './shared/shared.module';
 import { RecipesRoutingModule } from './recipes/recepies-rounting.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
@@ -38,7 +27,7 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(appRoutes),
+    RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules }),
     SharedModule,
     RecipesRoutingModule,
   ],
