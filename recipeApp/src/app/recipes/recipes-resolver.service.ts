9852211fe2ee0fa40347ca10ec11cc84f4b70e58
ridvanms <ridvans@abv.bg>
2022-09-11 22:50:09 +0300

    Fetching Recipes & Using the Resolver

--- recipeApp/src/app/recipes/recipes-resolver.service.ts
+++ recipeApp/src/app/recipes/recipes-resolver.service.ts
@@ -8,19 +8,27 @@ import {
 import { Recipe } from "./recipe.model";
 import { DataStorageService } from "../shared/data-storage.service";
 import { RecipeService } from "./recipe.service";
-
+import { Store } from "@ngrx/store";
+import * as FromApp from "../store/app.reducer";
+import * as RecipeActions from "./store/recipe.actions";
+import { Actions, ofType } from "@ngrx/effects";
+import { take } from "rxjs/operators";
 @Injectable({ providedIn: "root" })
 export class RecipesResolverService implements Resolve<Recipe[]> {
   constructor(
     private dataStorageService: DataStorageService,
-    private recipesService: RecipeService
+    private recipesService: RecipeService,
+    private store: Store<FromApp.AppState>,
+    private actions$: Actions
   ) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     const recipes = this.recipesService.getRecipes();
 
     if (recipes.length === 0) {
-      return this.dataStorageService.fetchRecipes();
+      // return this.dataStorageService.fetchRecipes();
+      this.store.dispatch(new RecipeActions.FetchRecipes());
+      return this.actions$.pipe(ofType(RecipeActions.SET_RECIPES), take(1));
     } else {
       return recipes;
     }
--- recipeApp/src/app/recipes/store/recipe.actions.ts
+++ recipeApp/src/app/recipes/store/recipe.actions.ts
@@ -1,12 +1,19 @@
+import { Actions } from "@ngrx/effects";
 import { Action } from "@ngrx/store";
 import { Recipe } from "../recipe.model";
 
 export const SET_RECIPES = "[Recipes] Set Recipes";
+export const FETCH_RECIPES = "[Recipe] Fetch Recipe";
 
 export class SetRecipes implements Action {
   readonly type = SET_RECIPES;
 
   constructor(public payload: Recipe[]) {}
+}
+export class FetchRecipes implements Action {
+  readonly type = FETCH_RECIPES;
+
+  constructor() {}
 }
 
 export type RecipesActions = SetRecipes;
