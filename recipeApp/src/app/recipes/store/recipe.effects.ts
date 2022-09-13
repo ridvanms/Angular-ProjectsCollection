8a59960540dfeb15fb7e7994d6c06afa9ee4822e
ridvanms <ridvans@abv.bg>
2022-09-13 22:41:59 +0300

    Storing Recipes via Effects

--- recipeApp/src/app/recipes/store/recipe.effects.ts
+++ recipeApp/src/app/recipes/store/recipe.effects.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Actions, Effect, ofType } from "@ngrx/effects";
-import { Action } from "@ngrx/store";
-import { switchMap, map } from "rxjs/operators";
+import { Action, Store } from "@ngrx/store";
+import { switchMap, map, withLatestFrom } from "rxjs/operators";
 import { Recipe } from "../recipe.model";
 
+import * as FromApp from "../../store/app.reducer";
 import * as RecipeActions from "./recipe.actions";
 
 @Injectable()
@@ -29,6 +30,21 @@ export class RecipeEffects {
       return new RecipeActions.SetRecipes(recipes);
     })
   );
+  @Effect({ dispatch: false })
+  storeRecipe = this.actions$.pipe(
+    ofType(RecipeActions.STORE_RECIPES),
+    withLatestFrom(this.store.select("recipes")),
+    switchMap(([actionData, recipesState]) => {
+      return this.http.put(
+        "https://recipebook-10645-default-rtdb.europe-west1.firebasedatabase.app/recipes.json",
+        recipesState.recipes
+      );
+    })
+  );
 
-  constructor(private actions$: Actions, private http: HttpClient) {}
+  constructor(
+    private actions$: Actions,
+    private http: HttpClient,
+    private store: Store<FromApp.AppState>
+  ) {}
 }
